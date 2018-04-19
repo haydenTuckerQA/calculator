85c1d4bd09c22614d9c6945ef7ec18252a472139
HaydenTuckerQA <Hayden.Tucker@qa.com>
2018-04-19 10:21:49 +0100

    Implemented  subtract using TDD

---
+++ src/test/java/com/qa/calculator/SubtractorTest.java
@@ -0,0 +1,19 @@
+package com.qa.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.qa.app.Subtractor;
+
+public class SubtractorTest {
+	
+	private Subtractor subtractor;
+
+	@Test
+	public void subtractTest() {
+		subtractor = new Subtractor();
+		int expectedValue = 2;
+		int actualValue = subtractor.subtract(4, 2);
+		Assert.assertEquals(expectedValue, actualValue);
+	}
+}
